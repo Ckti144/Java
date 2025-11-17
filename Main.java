b0fbfa5f2028098b29a5adff134cce84f1e5ccc0
Sakthi <sakthikrishkmv@gmail.com>
2025-11-17 16:05:21 +0530

    Python

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main{
     public static void main(String args[]){
-        System.out.println("Sakthi");
-        System.out.println("Murugan");
-    }
+       String [] name = new String[10];
+       name [0] ="Rudiger";
+       name [1] ="Benjimen Sesko";
+       name [2] ="Bryan Mbeumo";
+       for(int i = 0;i<=length.name;i++){
+        System.out.println(name);
+       }
+           }
 }
